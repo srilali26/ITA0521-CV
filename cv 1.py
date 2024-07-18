36fbe446afe5f780a0e54c33cd9682b1a01fde3b
srilali26 <124224140+srilali26@users.noreply.github.com>
2024-07-18 08:22:04 +0530

    Add files via upload

---
+++ cv 1.py
@@ -0,0 +1,10 @@
+import cv2
+import numpy as np
+kernel=np.ones((5,5),np.uint8)
+print(kernel)
+path=r"C:/Users/User/Pictures/Camera Roll/WIN_20231102_12_08_18_Pro.jpg"
+img=cv2.imread(path)
+imgGray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+cv2.imshow("GrayScale",imgGray)
+cv2.waitKey(0)
+
